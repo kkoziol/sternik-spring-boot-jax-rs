3a9f092f5f2b0b2bc3448027ee377d0223e48407
Krzysiek Koziol <kkoziol@gmail.com>
2018-03-25 11:49:21 +0200

    wersja z query param

--- src/main/java/pl/sternik/kk/rest/jaxrs/Store.java
+++ src/main/java/pl/sternik/kk/rest/jaxrs/Store.java
@@ -5,6 +5,7 @@ import javax.ws.rs.*;
 @Path("/store")
 public class Store {
 
+    //http://localhost:8080/store/article/5
     @GET
     @Path("/article/{id}")
     @Produces("application/json")
@@ -14,6 +15,19 @@ public class Store {
         article.setName("Mleko");
         return article;
     }
+
+    //http://localhost:8080/store/articles?id=6
+    @GET
+    @Path("/articles")
+    @Produces("application/xml")
+    public Article getArticles(@QueryParam("id") String id) {
+        Article article = new Article();
+        article.setId(Integer.valueOf(id));
+        article.setName("Mleko");
+        return article;
+    }
+
+
 
     @PUT
     @Path("/article")
